891ba142da1fe1be3c1082ec82b3682d3525adc1
jabinx92 <52295380+jabinx92@users.noreply.github.com>
2020-02-28 17:32:09 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  }
+  
+  var t1 = performance.now();
+  console.log(t1);
+  addUpTo(1000000000);
+  var t2 = performance.now();
+  console.log(t2);
+
+  console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+  function addUpTo(n) {
+    return n * (n + 1) / 2;
+  }
+  
+  var time1 = performance.now();
+  addUpTo(1000000000);
+  var time2 = performance.now();
+  console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+
+  //https://rithmschool.github.io/function-timer-demo/;
