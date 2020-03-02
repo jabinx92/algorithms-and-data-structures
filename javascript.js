3f0996f39377c90c63498e5bc6fda4aac21ca0cd
Jonathan Won <jabinx92@gmail.com>
2020-03-02 11:33:14 -0800

    big 0

--- javascript.js
+++ javascript.js
@@ -1,26 +1,45 @@
+/* BIG O NOTATION
+Time Complexity and Space complexity
+*/
+
 function addUpTo(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-      total += i;
-    }
-    return total;
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
   }
-  
-  var t1 = performance.now();
-  console.log(t1);
-  addUpTo(1000000000);
-  var t2 = performance.now();
-  console.log(t2);
+  return total;
+}
 
-  console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+var t1 = performance.now();
+console.log(t1);
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(t2);
 
-  function addUpTo(n) {
-    return n * (n + 1) / 2;
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+function addUpTo(n) {
+  return n * (n + 1) / 2;
+}
+
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+
+//https://rithmschool.github.io/function-timer-demo/;
+
+
+//example
+function addUpto(n){
+  let total = 0;
+  for(let i = 1; i <= n; i++) {
+    total += i;
   }
-  
-  var time1 = performance.now();
-  addUpTo(1000000000);
-  var time2 = performance.now();
-  console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+  return total;
+}
 
-  //https://rithmschool.github.io/function-timer-demo/;
+function addUpTo1(n) {
+  return n * (n + 1) / 2;
+}
+
