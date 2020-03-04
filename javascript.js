71f79692fdb8a4fb0df24347e083906971493499
Jonathan Won <jabinx92@gmail.com>
2020-03-04 12:38:03 -0800

    commit

--- javascript.js
+++ javascript.js
@@ -455,3 +455,5 @@ function averagePair(arr, num){
 }
 console.log(averagePair([1,2,3], 2.5)) //true (2+3 /2 = 2.5)
 console.log(averagePair([1,3,3,5,6,7,10,12,19], 8)) //true
+
+//---------------------------------------------------------
