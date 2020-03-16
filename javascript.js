1a524cc6638477ee836b142944efbcfb98cbadbc
Jonathan Won <jabinx92@gmail.com>
2020-03-16 00:01:58 -0700

    finish

--- javascript.js
+++ javascript.js
@@ -514,26 +514,49 @@ function isSubsequence(str1, str2) {
 //Time Complexity - O(N)
 //Space Complexity - O(1)
 
-function maxSubarraySum1(arr, num) {
-  //if num is greater than array length, return null
-  if (num > arr.length) {
-    return null;
-  }
+// function maxSubarraySum1(arr, num) {
+//   //if num is greater than array length, return null
+//   if (num > arr.length) {
+//     return null;
+//   }
 
-  //create a storage number
-  var num;
-  var num2;
+//   //create a storage number
+//   var num;
+//   var num2;
 
-  //loop over array
-  for(var i = 0; i < arr.length; i++) {
-    num2 = arr.slice(i, 2)
-    console.log(num2);
+//   //loop over array
+//   for(var i = 0; i < arr.length; i++) {
+//     num2 = arr.slice(i, 2)
+//     console.log(num2);
+//     var final = num2.reduce(x, y => {
+//       return x + y;
+//     })
+//     console.log(final);
+
   
-  }
+//   }
     
-  //return emptyNumber
-  return emptyNumber;
+//   //return emptyNumber
+//   return emptyNumber;
+// }
+
+//=============================ANSWER
+function maxSubarraySum(arr, num){
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i=0; i<num; i++){
+     total += arr[i];
+     console.log(total)
+  }
+  let currentTotal = total;
+  console.log(currentTotal)
+  for (let i = num; i < arr.length; i++) {
+     currentTotal += arr[i] - arr[i-num];
+     total = Math.max(total, currentTotal);
+  }
+  return total;
 }
 
-console.log(maxSubarraySum1([100,200,300,400], 2)) //700
-// console.log(maxSubarraySum1([1,4,2,10,23,3,1,0,20], 4)) //39
+console.log(maxSubarraySum([100,200,300,400], 2)) //700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)) //39
