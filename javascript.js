66ce67a3b222089c58f12d8f32e3348746c00a1a
Jonathan Won <jabinx92@gmail.com>
2020-03-18 00:36:05 -0700

    wrap up

--- javascript.js
+++ javascript.js
@@ -894,3 +894,19 @@ console.log(isPalindrome('foobar')) // false
 console.log(isPalindrome('tacocat')) // true
 console.log(isPalindrome('amanaplanacanalpanama')) // true
 console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+
+//===============================================================
+//someRecursive
+//write a recursive function called someRevursive which accepts an array and a callback.  the function returns true if a single value in the array returns true when passed to the callback. otherwise it returns false;
+
+// SAMPLE INPUT / OUTPUT
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(arr, callback){
+  // add whatever parameters you deem necessary - good luck!
+}
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
