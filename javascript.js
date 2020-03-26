0e1770baf7109cb2cbed896a0fd25cbe28f8756b
Jonathan Won <jabinx92@gmail.com>
2020-03-26 11:48:34 -0700

    quicksort

--- javascript.js
+++ javascript.js
@@ -1453,3 +1453,67 @@ console.log(mergeSort([10,24,76,72,1,9]))
 console.log(mergeSort([10,24,76,73]));
 console.log(mergeSort([5,1,1,2,0,0]));
 
+//quick sort
+//pivot helper - in order to implement merge sort, its useful to first implement a function responsible arranging elements in an array on either side of a pivor
+//given an array, this helper function should designate an element as the pivot 
+//it should then rearrange elements in the array so that all values less than the pivot are moved to the left of the pivot, and all values greater than the pivot are moved to the right of the pivot
+//the order of the elemts on either side of the pivot doesn't matter!
+//the helper should do this in place, that is,it should not create a new array
+//when complete, the helper should return the index of the pivot
+
+//picking a pivot
+//the runtime of the quick sort depends in part on how one selects the pivot
+//ideally, the pivot should be chosen so that it's roughly the median value in the data set you're sorting
+//for simplicity, we will always pick the first element(we'll talk about consequences of this later)
+
+//pivot pseudocode
+//itl help to accept three arguments: an array, a start index, and an end index (these can default to 0 and the array length minus 1, respectively)
+//grab the pivot from the start of the array
+//store the current pivot index in a varaiable(this will keep track of where the pivot should end up)
+//loop through the array from the start until the end
+  //if the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
+//swap the starting element (the pivot) with the pivot index
+
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+
+function quickSort(arr, left = 0, right = arr.length -1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right) //3
+        //left
+        quickSort(arr,left,pivotIndex-1);
+        //right
+        quickSort(arr,pivotIndex+1,right);
+      }
+     return arr;
+} 
+           
+quickSort([100,-3,2,4,6,9,1,2,5,3,23])
+
+// [4,6,9,1,2,5,3]
+// [3,2,1,4,6,9,5]
+//        4
+//  3,2,1    6,9,5
+//      3      6
+//  2,1      5  9
+//    2
