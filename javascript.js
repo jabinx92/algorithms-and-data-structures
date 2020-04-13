1b8544d4a0b5f4f17fa8bcb990eaa17c0aa17962
Jonathan Won <jabinx92@gmail.com>
2020-04-13 14:32:30 -0700

    breadth first search

--- javascript.js
+++ javascript.js
@@ -2175,73 +2175,86 @@ binary search tree - special case of binary tree - numbers are sorted in particu
 */
 
 
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-// class BinarySearchTree {
-//   constructor() {
-//     this.node = null;
-//   }
-//   insert(value) {
-//     var newNode = new Node(value);
-//     if(this.node === null) {
-//       this.node = newNode;
-//       return this;
-//     }
-//     let current = this.node;
-//     while(true) {
-//       if(value === current.value) return undefined;
-//       if(value < current.value) {
-//         if(current.left === null) {
-//           current.left = newNode;
-//           return this;
-//         } else {
-//           current = current.left;
-//         }
-//       } else if(value > current.value) {
-//         if(current.right === null){
-//           current.right = newNode;
-//           return this;
-//         } else {
-//           current = current.right
-//         }
-//       }
-//     }
-//   }
-//   find(value) {
-//     if(this.node === null) return false;
-//     let current = this.node;
-//     while(true) {
-//       if(current === null) return false;
-//       if(value === current.value) {
-//         return current;
-//       } else if(value < current.value) {
-//       current = current.left;
-//       } else {
-//       current = current.right;
-//       } 
-//     }
-//   }
-//   contains(value) {
-//     if(this.node === null) return false;
-//     let current = this.node;
-//     while(true) {
-//       if(current === null) return false;
-//       if(value === current.value) {
-//         return true;
-//       } else if(value < current.value) {
-//       current = current.left;
-//       } else {
-//       current = current.right;
-//       } 
-//     }
-//   }
-// }
+class BinarySearchTree {
+  constructor() {
+    this.node = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if(this.node === null) {
+      this.node = newNode;
+      return this;
+    }
+    let current = this.node;
+    while(true) {
+      if(value === current.value) return undefined;
+      if(value < current.value) {
+        if(current.left === null) {
+          current.left = newNode;
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else if(value > current.value) {
+        if(current.right === null){
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right
+        }
+      }
+    }
+  }
+  find(value) {
+    if(this.node === null) return false;
+    let current = this.node;
+    while(true) {
+      if(current === null) return false;
+      if(value === current.value) {
+        return current;
+      } else if(value < current.value) {
+      current = current.left;
+      } else {
+      current = current.right;
+      } 
+    }
+  }
+  contains(value) {
+    if(this.node === null) return false;
+    let current = this.node;
+    while(true) {
+      if(current === null) return false;
+      if(value === current.value) {
+        return true;
+      } else if(value < current.value) {
+      current = current.left;
+      } else {
+      current = current.right;
+      } 
+    }
+  }
+  BFS(){
+    var root = this.root;
+    var queue = [];
+    var result = [];
+    queue.push(root);
+    while(queue.length) {
+      var removed = queue.shift();
+      result.push(removed.value);
+      if(removed.left) queue.push(removed.left);
+      if(removed.right) queue.push(removed.right);
+    }
+    return result;
+  }
+}
 // var tree = new BinarySearchTree();
 // console.log(tree.insert(50));
 // console.log(tree.insert(5));
@@ -2253,6 +2266,7 @@ binary search tree - special case of binary tree - numbers are sorted in particu
 // console.log(tree.find(123));
 // console.log(tree.contains(55));
 // console.log(tree.contains(123));
+// console.log(tree.BFS());
 
 //this will take too long - SOLUTION - insert function
 // var tree = new BinarySearchTree();
@@ -2286,9 +2300,11 @@ There are two ways to sort through a unsorted tree.
 */
 
 //Breadth-first search
-  //create a queue (this can be an array) anda  variable to store the values of nodes visited
+  //create a queue (this can be an array) and  variable to store the values of nodes visited
   //place the root node in the queue
   //loop as long as there is anything in the queue
     //dequeue a node from the queue and push the value of the node into the variable that stores the nodes
     //if there is a left property on the node dequeued - add it to the queue
     //if there is a right property on the node dequeued - add it to the queue
+
+
