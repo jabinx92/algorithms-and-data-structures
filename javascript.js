c3503af96dbe738d38eeafbaace0133ca911f801
Jonathan Won <jabinx92@gmail.com>
2020-04-15 11:49:48 -0700

    start

--- javascript.js
+++ javascript.js
@@ -2366,3 +2366,35 @@ max binary heap -
 
   why are binary heaps important? binary heaps are used to implement priority queues, which are very commonly used data structures. they are also used quite a bit, with graph traversal algorithms
 */
+
+// class MaxBinaryHeap {
+//   constructor(){
+//     this.values = [41,39,33,18,27,12];
+//   }
+//   insert(element){
+//     this.values.push(element);
+//     this.bubbleUp();
+//   }
+//   bubbleUp(){
+//     let idx = this.values.length - 1;
+//     const element = this.values[idx];
+//     while(idx > 0){
+//       let parentIdx = Math.floor((idx - 1) / 2);
+//       let parent = this.values[parentIdx];
+//       if(element <= parent) break;
+//       this.values[parentIdx] = element;
+//       this.values[idx] = element;
+//       idx = parentIdx;
+//     }
+//   }
+// }
+
+// let heap = new MaxBinaryHeap();
+// console.log(heap.insert(55));
+// // [41,39,33,18,27,12,55]
+// //  0   1  2  3  4  5  6
+
+// // [41,39,33,18,27,12,55]
+// // [41,39,55,18,27,12,33]
+// // [55,39,41,18,27,12,33]
+// // [55,39,41,18,27,12,33]
