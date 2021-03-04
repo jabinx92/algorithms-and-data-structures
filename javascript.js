8b2d47c960e8cf4187e2ce1033cf19b7eaba8502
Jonathan Won <jabinx92@gmail.com>
2021-03-04 11:39:11 -0800

    leetcode 25

--- javascript.js
+++ javascript.js
@@ -2345,7 +2345,7 @@ tree.DFSPreOrder();
 tree.DFSPostOrder();
 tree.DFSInOrder();
 
-//leetcode 24
+//leetcode 25
 
 //this will take too long - SOLUTION - insert function
 // var tree = new BinarySearchTree();
